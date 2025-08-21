f469682ed54912084236937d7640057d10d171f2
alamgir-ahosain <alamgir.ahosain@gmail.com>
2025-08-21 10:07:44 +0600

    router to middleware

--- Backend/cmd/myApp/serve.go
+++ Backend/cmd/myApp/serve.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/alamgir-ahosain/e-commerce-project/internal/api/handlers"
 	"github.com/alamgir-ahosain/e-commerce-project/internal/api/routes"
 	"github.com/alamgir-ahosain/e-commerce-project/internal/middleware"
 	"github.com/alamgir-ahosain/e-commerce-project/internal/middleware/global"
@@ -18,7 +17,7 @@ func Serve() {
 	global.GlobalMiddleware(manager)
 
 	routes.RegisterRoutes(mux, manager)
-	globalRouter := handlers.GlobalRouter(mux) //handle CORS and OPTIONS method
+	globalRouter := middleware.CORSwithPreflight(mux) //handle CORS and OPTIONS method
 	fmt.Println("Server running on port :8080")
 	err := http.ListenAndServe(":8080", globalRouter)
 	if err != nil {
