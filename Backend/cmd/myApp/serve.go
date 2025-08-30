6683b93df92e3747b7747d6ae31dd4a3a76cd557
alamgir-ahosain <alamgir.ahosain@gmail.com>
2025-08-30 23:03:47 +0600

    refactor the file

--- Backend/cmd/myApp/serve.go
+++ Backend/cmd/myApp/serve.go
@@ -2,34 +2,18 @@ package myApp
 
 import (
 	"fmt"
-	"net/http"
-	"os"
 
 	"github.com/alamgir-ahosain/e-commerce-project/config"
-	"github.com/alamgir-ahosain/e-commerce-project/internal/api/routes"
-	"github.com/alamgir-ahosain/e-commerce-project/internal/middleware"
-	"github.com/alamgir-ahosain/e-commerce-project/internal/middleware/global"
+	"github.com/alamgir-ahosain/e-commerce-project/internal"
 )
 
 func Serve() {
 
 	//load config
 	cnf := config.GetConfig()
-	address := ":" + fmt.Sprint(cnf.HttpPort)
+	fmt.Println(cnf.Version)
+	fmt.Println(cnf.ServiceName)
+	fmt.Println(cnf.HttpPort)
 
-	// Middlewaere
-	manager := middleware.NewManager()
-	global.GlobalMiddleware(manager)
-
-	mux := http.NewServeMux()          //router
-	wrappedMux := manager.WrapMux(mux) //warp with Global Middleware
-	routes.RegisterRoutes(mux, manager)
-
-	fmt.Println("Server running on port", address)
-	// err := http.ListenAndServe(":8080", wrappedMux)
-	err := http.ListenAndServe(address, wrappedMux)
-	if err != nil {
-		fmt.Println("Error starting the server", err)
-		os.Exit(1)
-	}
+	internal.Start(cnf)
 }
