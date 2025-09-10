476d4eff9c233171aea4a547e17dcca8d2404b6b
alamgir-ahosain <alamgir.ahosain@gmail.com>
2025-09-10 19:19:19 +0600

    Create User

---
+++ Backend/internal/api/handlers/create_user.go
@@ -0,0 +1,27 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/alamgir-ahosain/e-commerce-project/internal/services"
+)
+
+// POST->header and body
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+
+	services.HandleCORSFunc(w)
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(200)
+		return
+	}
+	/*
+		1. take body information (description,price,title,imageUrl) from r.Body()
+		2.create an instance using Product struct with the body information
+		3.append the instance into productList
+	*/
+
+	// services.CreateProductFunc(w, r)
+	services.CreateUserFunc(w,r)
+
+
+}
