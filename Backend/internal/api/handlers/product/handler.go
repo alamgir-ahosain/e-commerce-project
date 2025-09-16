6b63bdb1580d8d1a621840f64033fb13bd8b9597
alamgir-ahosain <alamgir.ahosain@gmail.com>
2025-09-16 23:38:37 +0600

    Handler / products

--- Backend/internal/api/handlers/product/handler.go
+++ Backend/internal/api/handlers/product/handler.go
@@ -1,11 +1,18 @@
 package product
 
-import "github.com/alamgir-ahosain/e-commerce-project/internal/middleware"
+import (
+	"github.com/alamgir-ahosain/e-commerce-project/internal/middleware"
+	"github.com/alamgir-ahosain/e-commerce-project/internal/repository"
+)
 
 type Handler struct {
 	middlewares *middleware.Middlewares
+	productRepo repository.ProductRepo
 }
 
-func NewHandler(middlewares *middleware.Middlewares) *Handler {
-	return &Handler{middlewares: middlewares}
+func NewHandler(middlewares *middleware.Middlewares, productRepo repository.ProductRepo) *Handler {
+	return &Handler{
+		middlewares: middlewares,
+		productRepo: productRepo,
+	}
 }
